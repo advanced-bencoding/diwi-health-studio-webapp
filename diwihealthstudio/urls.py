a01e6c6850d1320495d459c0736d3d9eff5ddce9
advanced-bencoding <benjamin.joseph212620@gmail.com>
2022-02-21 22:16:52 +0530

    updated url imports

--- diwihealthstudio/urls.py
+++ diwihealthstudio/urls.py
@@ -18,10 +18,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-from services import views
-from home import views
+from services import views as services_view
+from home import views as home_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
+    path('', home_view.home, name='home'),
+    path('services/', services_view.services, name='services'),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
