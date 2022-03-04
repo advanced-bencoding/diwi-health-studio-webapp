7f97b027ddb9ad1d4fcdae8a41c62d7832d4b804
advanced-bencoding <benjamin.joseph212620@gmail.com>
2022-03-04 18:31:49 +0530

    increased char limit for name field

--- staff/models.py
+++ staff/models.py
@@ -3,6 +3,6 @@ from django.db import models
 
 # Create your models here.
 class Staff(models.Model):
-    image = models.ImageField(upload_to='images/')
-    name =  models.CharField(max_length=10)
+    image = models.ImageField(upload_to='images/staff/')
+    name =  models.CharField(max_length=100)
     qualifications = models.CharField(max_length=50)
