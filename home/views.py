02647be927b2fef6adf576007a01740bfe34ab01
advanced-bencoding <benjamin.joseph212620@gmail.com>
2022-03-04 18:29:47 +0530

    added staff to html dictionary

--- home/views.py
+++ home/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from services.models import Services, SubService
+from staff.models import Staff
 # Create your views here.
 def home(request):
     s = Services.objects
     sub = SubService.objects
-    return render(request, 'home/home.html', {'s': s, 'sub': sub})
+    staffs = Staff.objects
+    return render(request, 'home/home.html', {'s': s, 'sub': sub, 'staffs': staffs,})
