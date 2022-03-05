f27511877a5ef42c720c87099ce687fcaf85ad48
Raj <coder982923@gmail.com>
2022-03-06 01:21:04 +0530

    appointment input by user saved in db

--- appointment/models.py
+++ appointment/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Appointment (models.Model):
+    fname = models.CharField(max_length=50)
+    lname = models.CharField(max_length=50)
+    age = models.IntegerField()
+    sex = models.CharField(max_length=10)
+    mobile_no = models.CharField(max_length=13)
+    email = models.CharField(max_length=100)
+    date = models.DateTimeField()
+    time = models.CharField(max_length=100)
+    Notes = models.TextField()
+    service = models.CharField(max_length=20)
+
+    def __str__(self) :
+        return self.fname + self.lname
--- appointment/views.py
+++ appointment/views.py
@@ -1,9 +1,42 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .models import Appointment
 
 # Create your views here.
+
+
 def book(request):
-    return render(request, 'appointment/book.html')
+    if request.method == "POST":
+        if request.POST['fname'] and request.POST['lname'] and request.POST['age'] and request.POST['sex'] and request.POST['mobile'] and request.POST['email'] and request.POST['date'] and request.POST['service'] and request.POST['time'] :
+            a = Appointment()
+            a.fname = request.POST['fname']
+            a.lname = request.POST['lname']
+            a.age = request.POST['age']
+            a.date = request.POST['date']
+
+            if '@'and '.' in request.POST['email']:
+                a.email = request.POST['email']
+            else :
+                return render(request,'appointment/book.html',{'wrongmail':'Email is not valid, try again !!'})
+
+            if request.POST['mobile'].length<10 :
+                return render(request,'appointment/book.html',{'wrongno':'Mobile number is not valid , try again!!'})
+            else :
+                if request.POST['mobile'].startswith('+91'):
+                    a.mobile_no = request.POST['mobile']
+                else :
+                    a.mobile_no = '+91'+request.POST['mobile']
+
+            a.Notes = request.POST['notes']
+            a.service = request.POST['service']
+            a.sex = request.POST['sex']
+            a.time = request.POST['time']
+
+            a.save()
+            return render(request,'home/home.html',{'message':'Appointment Booked Successfully'})
+    else:
+        return render(request, 'appointment/book.html')
+
 
 @login_required
 def manage(request):
