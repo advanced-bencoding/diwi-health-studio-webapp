80febb91b83104922d0edd269f3980d1da0cad59
advanced-bencoding <benjamin.joseph212620@gmail.com>
2022-03-08 20:00:08 +0530

    new message for booked appointment

--- home/views.py
+++ home/views.py
@@ -1,9 +1,11 @@
+from email import message
 from django.shortcuts import render
 from services.models import Services, SubService
 from staff.models import Staff
 # Create your views here.
 def home(request):
+    message = request.GET.get('resp')
     s = Services.objects
     sub = SubService.objects
     staffs = Staff.objects
-    return render(request, 'home/home.html', {'s': s, 'sub': sub, 'staffs': staffs,})
+    return render(request, 'home/home.html', {'s': s, 'sub': sub, 'staffs': staffs, 'message': message})
