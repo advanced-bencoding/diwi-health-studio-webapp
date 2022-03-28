e374cc67982b9d7c859a822a0f1b67d24e0b4002
advanced-bencoding <benjamin.joseph212620@gmail.com>
2022-03-28 16:33:03 +0530

    now displaying author's name

--- blog/models.py
+++ blog/models.py
@@ -1,12 +1,13 @@
 from distutils.command.upload import upload
 from email.mime import image
 from django.db import models
-
+from staff.models import Staff
 class Blog(models.Model):
     title=models.CharField(max_length=100)
     description=models.TextField()
     date=models.DateField()
     image=models.ImageField(upload_to='images/blog/')
+    written_by= models.ForeignKey(Staff, on_delete=models.SET_NULL, null=True)
     
     def __str__(self):
         return self.title
